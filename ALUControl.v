2130152b81efb5312c3ab223af8149907b954543
Kenneth Hunter <kennethdhunter@gmail.com>
2019-10-17 09:00:39 -0600

    Restructured datapath file and alu control. Datapath is now 90% complete, but ALUcontrol will need slight modification to support jumps and branches. The current register approach for the program counter may also be better to move into a module that exactly mimics the PC in the datapath.

--- ALUControl.v
+++ ALUControl.v
@@ -1,66 +1,9 @@
-module ALUControl(instruction, r1PCIn, r2ImmediateIn, psrFlags, opControl, r1PCOut, r2ImmediateOut);
-	input [15:0] instruction, r1PCIn, r2ImmediateIn;
-	input [15:0] psrFlags;
-	output reg [7:0] opControl;
-	output reg [15:0] r1PCOut, r2ImmediateOut;
-
-	//Condition code params for ease of use
-	parameter EQ = 4'b0000, NE = 4'b0001, GT = 4'b0110, LE = 4'b0111, LT = 4'b1100, GE = 4'b1101;
-	//parameters for the PSR flag locations (rrrrIPE0NZF00LTC)
-	parameter Carry = 16'b1, LessThan = 16'b100, Overflow = 16'b100000, Zero = 16'b1000000, Negative = 16'b10000000,
-		Equal = 16'b1000000000; 
-
+module ALUControl(instruction, out);
+//maybe we send the PSR flags into here?
+	input [15:0] instruction;
+	output reg [7:0] out;
 	always @ (*) begin
-		//by default the provided input will be the output given to the ALU.
-		r1PCOut = r1PCIn;
-		r2ImmediateOut = r2ImmediateIn;
-		case(instruction[15:12])
-		//Need cases for Load, Store, SCond, BCond, and all Jumps.
-			4'b0100: begin //set condition, store and load, and all jumps
-				case(instruction[7:4])
-					4'b0000: begin //LOAD
-						//Need to take the value received from a provided address in memory.
-						//Once this value is obtained, the decoder should handle writing the value to the proper reg.
-						//Really just need the address added with zero. ADDU.
-						opControl = 8'b00000110;
-						r2ImmediateOut = 0;
-					end
-					4'b0100: begin //STORE
-						//Decoder should handle toggling the proper mux for an add between the register value and zero.
-						//This is really just an add U between r0 and the rsrc. This isn't quite going to work, but its a start.
-						opControl = 8'b00000110;
-						r2ImmediateOut = 0;
-					end
-					4'b1101: begin //SCond
-						//This is just a sneaky CMP hidden! It's also not technically needed, so I will wait until it is required.
-					end
-					4'b1100: begin //JCond
-						//IF the condition is true, we want to output the new value for the program counter. Need some way to check cond.
-						// Condition flags from first checkpoint will help.
-						// Good thing we have the PSR register flags. Still need the inputs though.
-						// Unless we use a NOP to waste time if the condition is false.
-						case(instruction[11:8])
-							EQ: begin
-								if(psrFlags & Equal) begin
-									opControl = 8'b00000000; //This needs to be changed.
-								end
-								else
-									//What to do here?
-									opControl = 8'b00000000;
-							end
-						endcase
-
-					end
-					default:
-						opControl = {instruction[15:12], instruction[7:4]};
-				endcase
-			end
-			4'b1100: begin //BCond
-				//Want to add an offset to the PC. This is an ADDI, using the PC and some address immediate.
-				opControl = 8'b01010000;
-			end
-			default:
-				opControl = {instruction[15:12], instruction[7:4]}; //By default, the op control will match the instruction. There are only a select few cases where this will not be true.
-		endcase
+		out = {instruction[15:12], instruction[7:4]}; //this is the case for most instructions.
+		//TODO handle jumps and branches. If the decoder can handle moving source values around, this might be easy!
 	end
 endmodule
--- Datapath.v
+++ Datapath.v
@@ -1,23 +1,40 @@
-module Datapath(clock, currentState, reset, regNormExtendCtl, reg2OrImmediateCtl, pcOrReg1Ctl, immediateSelectCtl);
-	input clock, reset, regNormExtendCtl, reg2OrImmediateCtl, pcOrReg1Ctl;
-	input [2:0] immediateSelectCtl;
-	input [1:0] currentState;
-	reg [15:0] instruction;
-	wire [15:0] regWriteData; //Not sure about this size...
-	wire [7:0] reg1Data, reg2Data, aluOperation;
-	reg [15:0] psrRegister;
-	wire [15:0] extendedR1Data, signExtendedImmediate, zeroExtendedImmediate, r1OutputToALU, r2OutputToALU, pcReg1OutputToALU, immediateMuxResult,
-	aluOutput, r1ALUIn, r2ALUIn;
-	wire regWriteEnable, zeroLine, carryLine, overflowLine, negativeLine, lowLine;
-	RegisterFile regFile(.clock(clock), .shouldWrite(regWriteEnable), .register1Address(instruction[15:12]), .register2Address(instruction[3:0]), .writeAddress(instruction[15:12]), .writeData(regWriteData), .register1Data(reg1Data), .register2Data(reg2Data));
-	SignExtender reg1Extend(.immediate(reg1Data), .extended(extendedR1Data));
-	SignExtender immediateSignExtend(.immediate(instruction[7:0]), .extended(signExtendedImmediate));
-	ZeroExtender immediateZeroExtend(.immediate(instruction[7:0]), .result(zeroExtendedImmediate));
-	mux2to1 regNormOrExtendMux(.in1(reg1Data), .in2(extendedR1Data), .select(regNormExtendCtl), .out(r1OutputToALU));
-	mux4to1 immediateSelectMux(.in1(instruction[7:0]), .in2(signExtendedImmediate), .in3(zeroExtendedImmediate), .in4(16'b1), .select(immediateSelectCtl), .out(immediateMuxResult));
-	mux2to1 reg2OrImmediateMux(.in1(reg2Data), .in2(immediateMuxResult), .select(reg2OrImmediateCtl), .out(r2OutputToALU));
-	mux2to1 pcOrReg1ResultMux(.in1(pcValue), .in2(r1OutputToALU), .select(pcOrReg1Ctl), .out(pcReg1OutputToALU));
-	ALUControl aluCtl(.instruction(instruction), .r1PCIn(pcReg1OutputToALU), .r2ImmediateIn(r2OutputToALU), .psrFlags(psrRegister), .opControl(aluOperation), .r1PCOut(r1ALUIn), .r2ImmediateOut(r2ALUIn));
-	ALU alu(.sourceData(pcReg1OutputToALU), .destData(r2OutputToALU), .operationControl(aluOperation), .carry(carryLine), .low(lowLine), .overflow(overflowLine), .zero(zeroLine), .negative(negativeLine), .result(aluOutput));
-	//psrRegister = {6'b000000, 1'b0, 1'b0, negativeLine, zeroLine, overflowLine, 2'b00, lowLine, 1'b0, carryLine};
+module Datapath(clock, reset, instruction, blockRamWriteEnable, registerFileWriteEnable, integerTypeSelectionLine, reg2OrImmediateSelectionLine, pcOrRegisterSelectionLine, addressFromRegOrDecoderSelectionLine, writeBackToRegRamOrALUSelectionLine, pcOrAluOutputRamReadSelectionLine, decoderRamWriteAddress);
+	input [15:0] instruction, decoderRamWriteAddress;
+	input clock, reset, reg2OrImmediateSelectionLine, pcOrRegisterSelectionLine, addressFromRegOrDecoderSelectionLine, 
+		writeBackToRegRamOrALUSelectionLine, blockRamWriteEnable, registerFileWriteEnable, pcOrAluOutputRamReadSelectionLine;
+	input [1:0] integerTypeSelectionLine;
+	//Setup Program Counter
+	reg [15:0] programCounter;
+	wire [15:0] signExtendedImmediate, zeroExtendedImmediate, selectedImmediateType, firstInputToAlu, secondInputToAlu, ramWriteAddress;
+	wire [15:0] reg1Data, reg2Data, ramReadData, aluOutput, regFileInputData, operationControlLine;
+	//Setup all needed muxes
+	mux4to1 integerTypeSelectionMux(.in1(instruction[7:0]), .in2(signExtendedImmediate), .in3(zeroExtendedImmediate), 
+		.in4(1), .select(integerTypeSelectionLine), .out(selectedImmediateType));
+	mux2to1 reg2OrImmediateMux(.in1(reg2Data), .in2(selectedImmediateType), .select(reg2OrImmediateSelectionLine), 
+		.out(secondInputToAlu));
+	mux2to1 pcOrRegisterMux(.in1(programCounter), .in2(reg1Data), .select(pcOrRegisterSelectionLine), 
+		.out(firstInputToAlu));
+	mux2to1 addressFromRegOrDecoderMux(.in1(reg2Data), .in2(decoderRamWriteAddress), .select(addressFromRegOrDecoderSelectionLine), 
+		.out(ramWriteAddress));
+	mux2to1 writeBackToRegRamOrALUMux(.in1(ramReadData), .in2(aluOutput), .select(writeBackToRegRamOrALUSelectionLine), 
+		.out(regFileInputData));
+	mux2to1 pcOrAluOutputRamReadMux(.in1(aluOutput), .in2(programCounter), .select(pcOrAluOutputRamReadSelectionLine), .out(ramReadAddress));
+	//End needed muxes
+	//Sign extender creation
+	SignExtender immediateSignExtender(.immediate(instruction[7:0]), .extended(signExtendedImmediate));
+	ZeroExtender immediateZeroExtender(.immediate(instruction[7:0]), .result(zeroExtendedImmediate));
+	//End extender creation
+	//Register File Creation
+	RegisterFile registerFile(.clock(clock), .shouldWrite(registerFileWriteEnable), .register1Address(instruction[11:7]), .register2Address(instruction[3:0]), 
+		.writeAddress(ramWriteAddress), .register1Data(reg1Data), .register2Data(reg2Data));
+	//End Register File Creation
+	//Begin Alu and controller creation
+	ALUControl controller(.instruction(instruction), .out(operationControlLine));
+	ALU alu(.sourceData(firstInputToAlu), .destData(secondInputToAlu), .operationControl(operationControlLine), .carry(), .low(), .overflow(), 
+		.zero(), .negative(), .result(aluOutput));
+	//End Alu and controller creation
+	//Block Ram Creation
+	BlockRam blockRam(.data(aluOutput), .read_addr(ramReadAddress), .write_addr(ramWriteAddress), .we(blockRamWriteEnable), .clk(clock), 
+		.q(ramReadData));
+
 endmodule
