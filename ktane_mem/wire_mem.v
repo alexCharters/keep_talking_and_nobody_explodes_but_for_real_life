48abc894d25d9e0554435360855db28e1755181d
edwardjohng@gmail.com <edwardjohng@gmail.com>
2019-11-12 00:34:11 -0700

    fixed wire mem to use a modified ADC controller and it uses the 8 most significant bits of the 12-bit adc output

--- ktane_mem/wire_mem.v
+++ ktane_mem/wire_mem.v
@@ -15,37 +15,37 @@ output	ADC_SDI;
 
 output reg [15:0] q;
 
-reg [11:0] ch [7:0];
+reg [11:0] ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch0;
 
-//adc_controller adc(.clk(clk),
-//	.ADC_CONVST(ADC_CONVST),
-//	.ADC_SCK(ADC_SCK),
-//	.ADC_SDI(ADC_SDI),
-//	.ADC_SDO(ADC_SDO),
-//	.ch(ch)
-//	);
+adc_controller adc(.clk(clk),
+	.ADC_CONVST(ADC_CONVST),
+	.ADC_SCK(ADC_SCK),
+	.ADC_SDI(ADC_SDI),
+	.ADC_SDO(ADC_SDO),
+	.ch0(ch0), .ch1(ch1), .ch2(ch2), .ch3(ch3), .ch4(ch4), .ch5(ch5), .ch6(ch6), .ch7(ch7)
+	);
 
 
 always @ (posedge clk)
 	begin
 		if(en) begin
 			if(addr >= 16'he664 && addr < 16'he886) begin
-				q <= ch[0];
+				q <= {2'h00, ch0[15:3]};
 			end
 			else if(addr >= 16'he886 && addr < 16'heaa8) begin
-				q <= ch[1];
+				q <= {2'h00, ch1[15:3]};
 			end
 			else if(addr >= 16'heaa8 && addr < 16'hecca) begin
-				q <= ch[2];
+				q <= {2'h00, ch2[15:3]};
 			end
 			else if(addr >= 16'hecca && addr < 16'heeec) begin
-				q <= ch[3];
+				q <= {2'h00, ch3[15:3]};
 			end
 			else if(addr >= 16'heeec && addr < 16'hf10e) begin
-				q <= ch[4];
+				q <= {2'h00, ch4[15:3]};
 			end
 			else if(addr >= 16'hf10e && addr < 16'hf330) begin
-				q <= ch[5];
+				q <= {2'h00, ch5[15:3]};
 			end
 		end
 	end
